c8e627baf154f9c050796e876f9be2a93247e27a
DharaniThirumalai <DharaniThirumalai@DESKTOP-T2OEFCC>
2025-07-21 20:47:52 +0530

    First program

---
+++ C2TC_Java/src/day1/Firstproject.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Firstproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java Class");
+	}
+	
+
+}
