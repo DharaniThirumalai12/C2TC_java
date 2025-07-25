8c5f06ec84b22198359771fad1c6c2df7f873b32
DharaniThirumalai <DharaniThirumalai@DESKTOP-T2OEFCC>
2025-07-25 14:14:30 +0530

    CoreJava

---
+++ C2TC_Java/src/com/tnsif/dayfive/multilevelinheritance/vehicle/Maruthi.java
@@ -0,0 +1,16 @@
+package com.tnsif.dayfive.multilevelinheritance.vehicle;
+
+
+	public class Maruthi extends Car {
+		public Maruthi() {
+			System.out.println("Class Maruthi");
+		}
+
+		public void brand() {
+			System.out.println("Brand: Maruthi");
+		}
+		public void speed() {
+			System.out.println("Max: 90Kmph");
+		}
+	}
+
