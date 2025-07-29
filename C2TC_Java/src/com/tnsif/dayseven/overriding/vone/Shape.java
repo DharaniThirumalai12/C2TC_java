6fd831ba1362b4a2b96e68c51c85702e9ad8cbc2
DharaniThirumalai <DharaniThirumalai@DESKTOP-T2OEFCC>
2025-07-29 10:30:06 +0530

    Core Java

---
+++ C2TC_Java/src/com/tnsif/dayseven/overriding/vone/Shape.java
@@ -0,0 +1,17 @@
+package com.tnsif.dayseven.overriding.vone;
+
+
+
+
+	public class Shape {
+
+		// Member function
+		public void draw() {
+			System.out.println("Drawing a generic shape");
+		}
+
+		public void erase() {
+			System.out.println("Erasing a generic shape");
+		}
+	
+}
