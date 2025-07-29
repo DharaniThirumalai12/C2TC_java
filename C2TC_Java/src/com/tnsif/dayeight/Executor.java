207ff60f18c993e62ac08495b1af7d51754e4f89
DharaniThirumalai <DharaniThirumalai@DESKTOP-T2OEFCC>
2025-07-29 11:56:50 +0530

    Core Java

--- C2TC_Java/src/com/tnsif/dayeight/Executor.java
+++ C2TC_Java/src/com/tnsif/dayeight/Executor.java
@@ -2,7 +2,7 @@ package com.tnsif.dayeight;
 
 public class Executor {
 public static void main (String[] args) {
-	Coder c1;
+	Coder c1;//This is reference
 	//Trainer object
 	c1=new Trainer();
 	c1.rules();
