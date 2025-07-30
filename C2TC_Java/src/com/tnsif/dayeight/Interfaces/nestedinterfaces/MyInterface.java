958f893cb5de9c64a382129ea5d12716d17b8103
DharaniThirumalai <DharaniThirumalai@DESKTOP-T2OEFCC>
2025-07-30 09:34:42 +0530

    Core Java

---
+++ C2TC_Java/src/com/tnsif/dayeight/Interfaces/nestedinterfaces/MyInterface.java
@@ -0,0 +1,10 @@
+package com.tnsif.dayeight.Interfaces.nestedinterfaces;
+
+public interface MyInterface {
+	 void calculateArea();
+	    interface MyInnerInterface {
+	       int  id = 20;
+	       void print();     
+	    }
+
+}
---
+++ C2TC_Java/src/com/tnsif/dayeight/Interfaces/nestedinterfaces/NestedInterfaceClass.java
@@ -0,0 +1,9 @@
+package com.tnsif.dayeight.Interfaces.nestedinterfaces;
+public class NestedInterfaceClass implements MyInterface.MyInnerInterface{
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		 System.out.println("Print method of nested interface");
+	}
+}
