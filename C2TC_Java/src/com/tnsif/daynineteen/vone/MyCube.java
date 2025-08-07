bbb60b21d21f3ed5061092f3a78e42e1fcacbd5b
DharaniThirumalai <DharaniThirumalai@DESKTOP-T2OEFCC>
2025-08-07 11:02:56 +0530

    Core Java

---
+++ C2TC_Java/src/com/tnsif/daynineteen/vone/MyCube.java
@@ -0,0 +1,6 @@
+package com.tnsif.daynineteen.vone;
+@FunctionalInterface
+public interface MyCube {
+	int getCube(int no);
+	//void show(); not allow more than one abstract method
+}
---
+++ C2TC_Java/src/com/tnsif/daynineteen/vone/Operations.java
@@ -0,0 +1,5 @@
+package com.tnsif.daynineteen.vone;
+@FunctionalInterface
+public interface Operations {
+	float performArithmetic(int a, int b);
+}
